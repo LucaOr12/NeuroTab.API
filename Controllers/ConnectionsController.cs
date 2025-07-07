2fc12349831f907c558f3390f7113d41040846a9
Luca Orabona <102247682+LucaOr12@users.noreply.github.com>
2025-07-07 10:29:05 +0200

    Add anonymous ping endpoint to ConnectionsController
    Introduced a new [HttpHead("ping")] endpoint that allows anonymous users to check the service health by returning 'pong'.

--- Controllers/ConnectionsController.cs
+++ Controllers/ConnectionsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using neurotab_api.Models;
 using neurotab_api.Data;
@@ -14,5 +15,12 @@ public class ConnectionsController : ControllerBase
     public ConnectionsController(NeuroTabContext context)
     {
         _context = context;
+    }
+    
+    [HttpHead("ping")]
+    [AllowAnonymous]
+    public IActionResult Ping()
+    {
+        return Ok("pong");
     }
 }
