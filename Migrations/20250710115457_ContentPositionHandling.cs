17c48862147ec5327876e3657a26902d101172ca
Luca Orabona <102247682+LucaOr12@users.noreply.github.com>
2025-07-10 13:55:36 +0200

    Add PositionX and PositionY to Content entity
    Introduces PositionX and PositionY float columns to the Contents table for handling content positioning. Includes migration and updates the model snapshot accordingly.

---
+++ Migrations/20250710115457_ContentPositionHandling.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace neurotab_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContentPositionHandling : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "PositionX",
+                table: "Contents",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "PositionY",
+                table: "Contents",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PositionX",
+                table: "Contents");
+
+            migrationBuilder.DropColumn(
+                name: "PositionY",
+                table: "Contents");
+        }
+    }
+}
